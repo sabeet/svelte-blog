c457434b2311222a0423d5177f6ab6af29817e17
sabeet <sabeet.other.way@gmail.com>
2023-06-18 18:25:41 -0400

    testing testing 1

---
+++ src/Post.svelte
@@ -0,0 +1,6 @@
+<script>
+    export let post;
+</script>
+    
+    <h2>{post.title}</h2>
+    <p>{post.content}</p>
---
+++ src/blog.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Post from './Post.svelte';
+    
+    export let posts;
+    </script>
+    
+    <ul>
+    {#each posts as post}
+      <li>
+        <Post {post} />
+      </li>
+    {/each}
+    </ul>
